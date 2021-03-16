669981647f9f7eee951095de3e9660d83dd9ae96
marialioubakova <59501464+marialioubakova@users.noreply.github.com>
2021-03-16 10:31:13 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+
+from flask import Flask
+from flask_ngrok import run_with_ngrok
+
+app = Flask(__name__)
+run_with_ngrok(app)
+
+@app.route("/")
+def index():
+    return "Привет от приложения Flask"
+
+
+if __name__ == '__main__':
+    # os.environ пытается получить значение переменной окружения с именем PORT, а
+    # если не получается, то выставляет порт 5000
+    # port = int(os.environ.get("PORT", 5000))
+    # app.run(host='0.0.0.0', port=port)
+    app.run()
